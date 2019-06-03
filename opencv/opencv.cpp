eb25206b206d323260f82f04c2c5bc1b03279c68
liang tail <techtxl123@outlook.com>
2019-06-03 09:55:56 +0800

    test opencv

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -1,8 +1,18 @@
-
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat img = imread("C:\\Users\\tailiang\\Pictures\\images.jpg");
+	if (img.empty())
+	{
+		return -1;
+	}
+	namedWindow("example", WINDOW_AUTOSIZE);
+	imshow("example", img);
+	waitKey(0);
+	destroyWindow("example");
+	return 0;
 }
-
